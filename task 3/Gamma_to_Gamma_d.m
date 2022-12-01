4cb4aaefb56d94202be0122364a33cb856761494
João Da Fonseca <84569788+JoaoDaFonsecaMC@users.noreply.github.com>
2022-12-01 22:50:20 +0000

    Create Gamma_to_Gamma_d.m

---
+++ task 3/Gamma_to_Gamma_d.m
@@ -0,0 +1,33 @@
+function [Gamma_d,error] = Gamma_to_Gamma_d (Gamma,A,h,epsilon,max_it)
+
+    [N,error] = Norm (A,h,epsilon,max_it);
+    %only run if the norm did found a solution
+    if error == 0
+        
+        Gamma_sum = eye(size(A));
+        
+        for j = 1:N
+           
+            Gamma_sum = Gamma_sum + (A^(j-1)*h^(j)) / factorial(j);
+        end
+        
+        Gamma_d = Gamma_sum * Gamma;
+    end
+
+    function [N,error] = Norm (A,h,epsilon,max_it)
+        N = 1;
+        fro = epsilon + 10;
+        
+        while i<= max_it || N >= epsilon
+            m = (A*h)^(N)/ factorial(N);
+            fro = norm( m, 'fro');
+            N = N +1;
+        end
+        %check for solution and give error
+        if fro < epsilon
+            error = 0;
+        else
+            error = 1;
+        end
+    end
+end
